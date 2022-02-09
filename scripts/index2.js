48ce4f9ba2fc35ae0deae1410a8e310c5b4c5f52
mengiefen <mengiefen@gmail.com>
2022-02-09 15:30:00 +0300

    add header in index.html

--- scripts/index2.js
+++ scripts/index2.js
@@ -1,7 +1,7 @@
-const bookShelf = document.getElementById('book-shelf');
-const addButton = document.getElementById('btn-add');
-const titleInput = document.getElementById('title');
-const authorInput = document.getElementById('author');
+const bookShelf = document.getElementById("book-shelf");
+const addButton = document.getElementById("btn-add");
+const titleInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
 
 class Book {
   constructor(title, author, id) {
@@ -10,9 +10,9 @@ class Book {
     this.author = author;
   }
 
-  Books = []
+  Books = [];
 
-  // Add a single book and store in Book
+  // Adds a single book and store in Book
   addBook() {
     this.Books.push({
       id: this.id,
@@ -22,16 +22,16 @@ class Book {
     return this.Books;
   }
 
-  // Read books from local storage
+  // Reads books from local storage
   readBooks() {
-    const bookSaved = JSON.parse(localStorage.getItem('books'));
+    const bookSaved = JSON.parse(localStorage.getItem("books"));
     if (bookSaved) {
       this.Books = bookSaved;
     }
     return this.Books;
   }
 
-  // Remove a single move from local storage
+  // Removes a single move from local storage
   removeBook(id) {
     let Books = this.readBooks();
     Books = Books.filter((book) => book.id !== Number(id));
@@ -40,28 +40,28 @@ class Book {
     return Books;
   }
 
-  // Write the books list into the localStorage
+  // Writes the books list into the localStorage
   writeBooks() {
-    localStorage.setItem('books', JSON.stringify(this.Books));
+    localStorage.setItem("books", JSON.stringify(this.Books));
   }
 }
 
 const Buk = new Book();
 
-// Render each book with single row
+// Renders each book with single row
 const displayBook = (book) => {
   const bookHolderHTML = `  
     <h4 class="book-title"><em>"${book.title}" by <span>${book.author}</span></em> </h4>  
     <button class="btn-remove" id=${book.id}>Remove</button>`;
-  const bookHolder = document.createElement('div');
-  bookHolder.classList.add('book-holder');
+  const bookHolder = document.createElement("div");
+  bookHolder.classList.add("book-holder");
   bookHolder.innerHTML = bookHolderHTML;
   bookShelf.appendChild(bookHolder);
 };
 
 // Reads all books from local storage and renders the result
 const drawAllBooks = () => {
-  bookShelf.innerHTML = '';
+  bookShelf.innerHTML = "";
   const Books = Buk.readBooks();
   Books.forEach((element, index) => {
     if (index < 25) {
@@ -76,23 +76,28 @@ const addBook = () => {
   Buk.title = titleInput.value;
   Buk.author = titleInput.value;
   Buk.id = Date.now();
-  if (titleInput.value !== '' && authorInput.value !== '') {
+  if (titleInput.value !== "" && authorInput.value !== "") {
     Buk.addBook();
     Buk.writeBooks();
     displayBook({
       title: titleInput.value,
       author: authorInput.value,
     });
-    titleInput.value = '';
-    authorInput.value = '';
+    titleInput.value = "";
+    authorInput.value = "";
   }
 };
+// Event listener for 'Add new Button'
+addButton.addEventListener("click", addBook);
 
-addButton.addEventListener('click', addBook);
-
-document.body.addEventListener('click', (event) => {
-  if (event.target.className === 'btn-remove') {
-    Buk.removeBook(event.target.id);
-    drawAllBooks();
-  }
-}, false);
+// Event listener for 'Add new Button'
+document.body.addEventListener(
+  "click",
+  (event) => {
+    if (event.target.className === "btn-remove") {
+      Buk.removeBook(event.target.id);
+      drawAllBooks();
+    }
+  },
+  true
+);
